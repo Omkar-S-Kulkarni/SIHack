2b020adc67c7d7b99eb5e647b42db43888ae57cc
Violane-7 <smeraldo223@gmail.com>
2025-09-18 11:32:06 +0530

    Added for main page

---
+++ my-app/src/App1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainPage from "./pages/mainPage/MainPage";
+
+function App1() {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
+}
+
+export default App1;
